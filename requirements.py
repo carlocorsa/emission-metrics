fd1d2abbce6eac749aa3427b7b8cc355438b2fe4
carlocorsa <carlo.corsaro@gmail.com>
2020-06-16 19:06:07 +0100

    Updated requirements files

--- requirements.py
+++ requirements.py
@@ -1,3 +1,5 @@
 pip install numpy
+pip install scipy
 pip install netCDF4
+pip install matplotlib
 
--- requirements_with_version.py
+++ requirements_with_version.py
@@ -1,2 +1,4 @@
 pip install numpy==1.18.4
+pip install scipy==1.4.1
 pip install netCDF4==1.5.3
+pip install matplotlib==3.1.3
