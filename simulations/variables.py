484babd2394e444a7c9b68c7db3c65fbda736171
carlocorsa <carlo.corsaro@gmail.com>
2020-06-18 12:03:39 +0100

    Added module with functions to compute various variables

--- simulations/variables.py
+++ simulations/variables.py
@@ -2,10 +2,11 @@
 import numpy as np
 
 # Local application imports
-from rem.simulations import loading, regions
+from rem.utils import constants
+from rem.simulations import loading, regions, input_selection, scaling
 
 
-def compute_variables(response_region, grid_delta_temp, grid_delta_precip):
+def compute_climate_variables(response_region, grid_delta_temp, grid_delta_precip):
     """Compute average regional and global temperature and precipitation
     variations between perturbation and control experiments.
 
@@ -54,3 +55,90 @@ def compute_variables(response_region, grid_delta_temp, grid_delta_precip):
     precip_avg = np.ma.sum(np.ma.sum(grid_delta_precip * areas)) / np.ma.sum(np.ma.sum(areas))
 
     return rr_temp_avg, temp_avg, rr_precip_avg, precip_avg
+
+
+def compute_radiative_efficiency(pollutant, emission_region, response_region):
+    """Compute radiative efficiencies.
+
+    Parameters
+    ----------
+    pollutant: str
+        One of the following four options:
+        - SO2
+        - BC
+        - CO2
+        - CH4
+
+    emission_region: str
+        The name of the pollutant emission region.
+        For SO2, CO2 and CH4, one of the following options:
+        - NHML
+        - US
+        - China
+        - EastAsia
+        - India
+        - Europe
+
+        For BC, one of the following options:
+        - Global
+        - Asia
+
+    response_region: str
+        Name of the response region.
+
+    Returns
+    -------
+    rr_rad_eff: float
+        Regional radiative efficiency.
+
+    rad_eff: float
+        Global radiative efficiency.
+
+    rad_eff_a: float
+        Atmospheric component of the global
+        radiative efficiency.
+    """
+
+    assert pollutant in constants.POLLUTANTS, "{} is not an accepted pollutant".format(pollutant)
+
+    # Get the effective radiative forcing
+    _, erf, erf_a = input_selection.select_emission_region(pollutant, emission_region)
+
+    # Get the pollutant emission mass
+    delta_emiss_mass = loading.get_emissions(pollutant, emission_region)
+
+    # Get the gridded climate responses
+    grid_delta_temp, grid_delta_precip = loading.load_variables(pollutant, emission_region)
+
+    # Compute average climate variables
+    rr_temp_avg, temp_avg, rr_precip_avg, precip_avg = compute_climate_variables(
+        response_region, grid_delta_temp, grid_delta_precip
+    )
+
+    if pollutant == 'SO2':
+        rr_rad_eff = ((erf * scaling.get_mm_scaling(pollutant, 'temperature')[1]) * (rr_temp_avg / temp_avg) /
+                      (delta_emiss_mass * constants.SPECS[pollutant]['tau']))
+
+        rad_eff = ((erf * scaling.get_mm_scaling(pollutant, 'temperature')[1]) /
+                   (delta_emiss_mass * constants.SPECS[pollutant]['tau']))
+
+        rad_eff_a = ((erf_a * scaling.get_mm_scaling(pollutant, 'temperature')[1]) /
+                     (delta_emiss_mass * constants.SPECS[pollutant]['tau']))
+
+    elif pollutant == 'CO2':
+        rr_rad_eff = erf * (rr_temp_avg / temp_avg) / delta_emiss_mass
+
+        rad_eff = erf / delta_emiss_mass
+
+        rad_eff_a = erf_a * delta_emiss_mass
+
+    else:
+        rr_rad_eff = erf * (rr_temp_avg / temp_avg) / (delta_emiss_mass * constants.SPECS[pollutant]['tau'])
+
+        rad_eff = erf / (delta_emiss_mass * constants.SPECS[pollutant]['tau'])
+
+        rad_eff_a = erf_a / (delta_emiss_mass * constants.SPECS[pollutant]['tau'])
+
+    return rr_rad_eff, rad_eff, rad_eff_a
+
+
