a546443d36ab7e42961748e9364295a5f9b143aa
carlocorsa <carlo.corsaro@gmail.com>
2020-06-23 11:32:46 +0100

    Added function docstrings, fixed isssue with CO2 emissions

--- scenarios/temperature_scenarios.py
+++ scenarios/temperature_scenarios.py
@@ -10,21 +10,109 @@ A0 = constants.SPECS['CO2']['a0']
 Ai = constants.SPECS['CO2']['ai']
 
 
-def compute_time_step_temperature(t, emiss_scenario, artp, time_step=0.01):
-    if t == 0:
-        return emiss_scenario[t] * (time_step * artp[t] / 2)
+def compute_time_step_temperature(index, emissions, artp, time_step=0.01):
+    """"Compute temperature change by numerical integration.
+
+    Parameters
+    ----------
+    index: int
+        Index at which to compute the temperature change.
+
+    emissions: list of floats
+        List of emission values.
+
+    artp: list of floats
+        List of ARTP values.
+
+    time_step: float (default=0.01)
+        Length in years of the time step used
+        for numerical integration.
+
+    Returns
+    -------
+    Temperature change at `index`.
+    """
+    if index == 0:
+        return emissions[index] * (time_step * artp[index] / 2)
     else:
-        return sum([emiss_scenario[i] * (time_step * (artp[t - i] + artp[t - i - 1]) / 2) for i in range(0, t)])
+        return sum([emissions[i] * (time_step * (artp[index-i] + artp[index-i-1]) / 2) for i in range(0, index)])
 
 
 def compute_mixed_scenarios_temperature(
         pollutant, emiss_region, magnitudes, emiss_scenarios, time_horizons, artp, time_step=0.01
 ):
+    """Compute the temperature change due to variations of `pollutant' emissions.
+    There can be any number of emission scenarios and each of them needs to have magnitude,
+    scenario type and time horizon specified.
+
+    Parameters
+    ----------
+    pollutant: str
+        One of the following four options:
+        - SO2
+        - BC
+        - CO2
+        - CH4
+
+    emiss_region: str
+        The name of the pollutant emission region.
+        For SO2, CO2 and CH4, one of the following options:
+        - NHML
+        - US
+        - China
+        - EastAsia
+        - India
+        - Europe
+
+        For BC, one of the following options:
+        - Global
+        - Asia
+
+    magnitudes: list of integers
+        A list of magnitudes, one for each scenario.
+           0 = total reduction (zero emission)
+         100 = no change (100% of current emission)
+        1000 = 10 times current emissions
+
+    emiss_scenarios: list of str
+        A list of emission scenario types.
+        Must be one of the following:
+        - linear
+        - sustained
+        - quadratic
+
+    time_horizons: list of integers
+        A list of time horizons, one for each scenario.
+        Each time horizon must be larger than the
+        previous one, eg, [30, 60, 100].
+
+    artp: list of floats
+        A list of ARTP values covering the maximum
+        time horizon considered.
+        Must have a length equal to:
+        max(time_horizons) * (1 / time_step)
+
+    time_step: float (default=0.01)
+        Length in years of the time step used
+        for numerical integration.
+        Should be smaller than the lifetime of the
+        used pollutant.
+
+    Returns
+    -------
+    mixed_scenario_temperature: list of floats
+        List of temperature changes for each point in
+        the period considered.
+        Length = max(time_horizons) * (1 / time_step)
+    """
 
     assert len(emiss_scenarios) == len(time_horizons), \
         "The number of scenarios ({}) does not correspond to the number of time horizons ({}).".format(
             len(emiss_scenarios), len(time_horizons)
         )
+
+    for scenario in emiss_scenarios:
+        assert scenario in constants.SCENARIOS, "{} is not a valid scenario type".format(scenario)
 
     # Compute number of steps per year
     n_steps = int(1 / time_step)
@@ -57,20 +145,24 @@ def compute_mixed_scenarios_temperature(
 
         # Scale CO2 emissions
         if pollutant == 'CO2':
+            dth_i = time_horizons[i] - time_horizons[i-1] if i > 0 else time_horizons[i]
             aai = np.array([A0] + Ai)
             atau = np.array(
                 [time_horizons[0]] +
-                [ti if ti < time_horizons[0] else time_horizons[0] for ti in constants.SPECS['CO2']['tau']])
-            delta_emiss_mass = delta_emiss_mass / sum(aai * atau)
+                [ti if ti < dth_i else dth_i for ti in constants.SPECS['CO2']['tau']]
+            )
+            tot_delta_emiss_mass = delta_emiss_mass / sum(aai * atau)
+        else:
+            tot_delta_emiss_mass = delta_emiss_mass
 
         if i == 0:
             prev_th = 0
             prev_scen_emiss = 0
-            scen_emiss = sign * delta_emiss_mass * (magnitudes[i] - 100) / 100
+            scen_emiss = sign * tot_delta_emiss_mass * (magnitudes[i] - 100) / 100
         else:
             prev_th = time_horizons[i - 1] * n_steps
-            prev_scen_emiss = sign * delta_emiss_mass * (magnitudes[i - 1] - 100) / 100
-            scen_emiss = sign * delta_emiss_mass * (magnitudes[i] - magnitudes[i - 1]) / 100
+            prev_scen_emiss = sign * tot_delta_emiss_mass * (magnitudes[i-1] - 100) / 100
+            scen_emiss = sign * tot_delta_emiss_mass * (magnitudes[i] - magnitudes[i-1]) / 100
 
         if emiss_scenarios[i] == 'linear':
             for t in range(prev_th, curr_th):
