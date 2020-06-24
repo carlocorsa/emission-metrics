3065dab3004886a745fc95a3fbe698742c14c917
carlocorsa <carlo.corsaro@gmail.com>
2020-06-24 16:31:08 +0100

    Added pandas to requirements

--- requirements.py
+++ requirements.py
@@ -2,4 +2,5 @@ pip install numpy
 pip install scipy
 pip install netCDF4
 pip install matplotlib
+pip install pandas
 
--- requirements_with_version.py
+++ requirements_with_version.py
@@ -2,3 +2,5 @@ pip install numpy==1.18.4
 pip install scipy==1.4.1
 pip install netCDF4==1.5.3
 pip install matplotlib==3.1.3
+pip install pandas==1.0.5
+
