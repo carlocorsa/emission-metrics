ac845aeba4a6696ee1ce19edb828750921d7a1cc
carlocorsa <carlo.corsaro@gmail.com>
2020-06-29 15:15:05 +0100

    Removed get_climate_stats function, added docstring to get_potential_uncertainties, fixed minor issues

--- uncertainties/propagation.py
+++ uncertainties/propagation.py
@@ -3,144 +3,90 @@ import pandas as pd
 
 # Local application imports
 from rem.simulations import input_selection, scaling
-from rem.uncertainties import ctl_runs, erf
-from rem.utils import stats, constants
+from rem.uncertainties import erf, climate_variables
+from rem.utils import constants
 
 
-def get_climate_stats(response_region):
-    """Get regional and global temperature and precipitation stats.
+def get_potential_uncertainties(pollutant, emission_region, response_region):
+    """Get propagated uncertainties for the ARTP and the ARPP.
+    N.B. To get the full uncertainties the returned values will need to be
+    multiplied by the absolute value of the corresponding potential.
 
     Parameters
     ----------
+    pollutant: str
+        One of the following four options:
+        - SO2
+        - BC
+        - CO2
+        - CH4
+
+    emission_region: str
+        The name of the pollutant emission region.
+        For SO2, CO2 and CH4, one of the following options:
+        - NHML
+        - US
+        - China
+        - EastAsia
+        - India
+        - Europe
+
+        For BC, one of the following options:
+        - Global
+        - Asia
+
     response_region: str
         Name of the response region.
-        If response_region='All regions', stats will be
-        returned for all response regions.
 
     Returns
     -------
-    reg_temp_avg: float or list
-        Regional average temperature or list of regional average
-        temperatures if response_region='All regions.
+    artp_total_std: float or list of floats
+        Propagated uncertainty for the ARTP.
+        Can be either a single float for a single response region or
+        a list of floats if response_region='All regions'.
 
-    glo_temp_avg: float
-        Global average temperature.
+    slow_arpp_total_std: float or list of floats
+        Propagated uncertainty for the slow component of the ARPP.
+        Can be either a single float for a single response region or
+        a list of floats if response_region='All regions'.
 
-    temp_ratio_std_err: float or list
-        Standard error of the ratio between regional and global average temperatures
-        list of standard errors if response_region='All regions.
-
-    reg_precip_avg: float or list
-        Regional average precipitation or list of regional average
-        precipitations if response_region='All regions.
-
-    glo_precip_avg: float
-        Global average precipitation.
-
-    precip_ratio_std_err: float or list
-        Standard error of the ratio between regional and global average precipitations
-        or list of standard errors if response_region='All regions.
+    fast_arpp_total_std: float or list of floats
+        Propagated uncertainty for the fast component of the ARPP.
+        Can be either a single float for a single response region or
+        a list of floats if response_region='All regions'.
     """
 
-    if isinstance(response_region, list):
-        response_region = 'All regions'
+    assert pollutant in constants.POLLUTANTS, "{} is not an accepted pollutant".format(pollutant)
 
-    # Get temperature and precipitation intermodel variability
-    temp_df, precip_df = ctl_runs.get_model_variability(response_region)
-
-    glo_temp_avg = temp_df.loc['Global', 'avg']
-    glo_temp_std_err = temp_df.loc['Global', 'std_err']
-    glo_precip_avg = precip_df.loc['Global', 'avg']
-    glo_precip_std_err = precip_df.loc['Global', 'std_err']
-
-    if response_region == 'All regions':
-
-        region_names = input_selection.get_response_regions()
-        n_regions = len(region_names)
-
-        reg_temp_avg = []
-        reg_temp_std_err = []
-        temp_ratio_std_err = []
-        reg_precip_avg = []
-        reg_precip_std_err = []
-        precip_ratio_std_err = []
-
-        for i in range(n_regions):
-
-            # Compute regional temperature average, standard error and covariance with global temperature
-            reg_temp_avg.append(temp_df.loc[region_names[i], 'avg'])
-            reg_temp_std_err.append(temp_df.loc[region_names[i], 'std_err'])
-            reg_glo_temp_cov = stats.compute_covariance(
-                temp_df.loc[region_names[i]][0:6], temp_df.loc['Global'][0:6], std_err=True
-            )
-
-            # Compute regional precipitation average, standard error and covariance with global precipitation
-            reg_precip_avg.append(precip_df.loc[region_names[i], 'avg'])
-            reg_precip_std_err.append(precip_df.loc[region_names[i], 'std_err'])
-            reg_glo_precip_cov = stats.compute_covariance(
-                precip_df.loc[region_names[i]][0:6], precip_df.loc['Global'][0:6], std_err=True
-            )
-
-            # Compute standard error of regional and global temperature ratio
-            temp_ratio_std_err.append(
-                stats.compute_ratio_std(
-                    reg_temp_avg, glo_temp_avg, reg_temp_std_err, glo_temp_std_err, reg_glo_temp_cov
-                )
-            )
-
-            # Compute standard error of regional and global precipitation ratio
-            precip_ratio_std_err.append(
-                stats.compute_ratio_std(
-                    reg_precip_avg, glo_precip_avg, reg_precip_std_err, glo_precip_std_err, reg_glo_precip_cov
-                )
-            )
-
+    if pollutant == 'BC':
+        assert emission_region in constants.BC_EMISS_REGIONS, \
+            "{} is not an accepted emission region for {}".format(emission_region, pollutant)
     else:
+        assert emission_region in constants.EMISS_REGIONS, \
+            "{} is not an accepted emission region for {}".format(emission_region, pollutant)
 
-        reg_temp_avg = temp_df.loc[response_region, 'avg']
-        reg_temp_std_err = temp_df.loc[response_region, 'std_err']
-        reg_glo_temp_cov = stats.compute_covariance(
-            temp_df.loc['Europe'][0:6], temp_df.loc['Global'][0:6], std_err=True
-        )
-
-        reg_precip_avg = precip_df.loc[response_region, 'avg']
-        reg_precip_std_err = precip_df.loc[response_region, 'std_err']
-        reg_glo_precip_cov = stats.compute_covariance(
-            precip_df.loc['Europe'][0:6], precip_df.loc['Global'][0:6], std_err=True
-        )
-
-        temp_ratio_std_err = stats.compute_ratio_std(
-            reg_temp_avg, glo_temp_avg, reg_temp_std_err, glo_temp_std_err, reg_glo_temp_cov
-        )
-        precip_ratio_std_err = stats.compute_ratio_std(
-            reg_precip_avg, glo_precip_avg, reg_precip_std_err, glo_precip_std_err, reg_glo_precip_cov
-        )
-
-    return reg_temp_avg, glo_temp_avg, temp_ratio_std_err, reg_precip_avg, glo_precip_avg, precip_ratio_std_err
-
-
-def error_prop(pollutant, k, fp, opath, gpath, response_region, emission_region):
-
+    fp = constants.SPECS[pollutant]['fp']
+    k = constants.SPECS[pollutant]['k']
     k_std = constants.SPECS[pollutant]['k_std']
 
     # Get temperature and precipitation stats
     reg_temp_avg, glo_temp_avg, temp_ratio_std_err, \
-        reg_precip_avg, glo_precip_avg, precip_ratio_std_err = get_climate_stats(response_region)
+        reg_precip_avg, glo_precip_avg, precip_ratio_std_err = climate_variables.get_climate_stats(response_region)
 
     # Get ERF regional uncertainties
-    if pollutant == 'CO2' or pollutant == 'CH4':
-        reg_erf_avg = 1
-        reg_erf_std_err = 0
-        reg_erfa_avg = 1
-        reg_erfa_std_err = 0
+    if pollutant == 'BC':
+        reg_erf_avg, reg_erf_std_err, reg_erfa_avg, reg_erfa_std_err = erf.get_bc_regional_uncertainty(emission_region)
 
     elif pollutant == 'SO2':
-        reg_erf_avg, reg_erf_std_err = erf.get_so2_regional_uncertainty(opath, gpath, pollutant, emission_region)
+        reg_erf_avg, reg_erf_std_err = erf.get_so2_regional_uncertainty(emission_region)
         reg_erfa_avg = fp * reg_erf_avg
         reg_erfa_std_err = np.abs(fp) * reg_erf_std_err
 
     else:
-        reg_erf_avg, reg_erf_std_err, reg_erfa_avg, reg_erfa_std_err = erf.get_bc_regional_uncertainty(emission_region)
+        reg_erf_avg = 1
+        reg_erf_std_err = 0
+        reg_erfa_avg = 1
+        reg_erfa_std_err = 0
 
     # Get ERF global uncertainties
     glo_erf_avg, glo_erf_std_err, glo_erfa_avg, glo_erfa_std_err = erf.get_global_uncertainty(pollutant)
@@ -150,10 +96,14 @@ def error_prop(pollutant, k, fp, opath, gpath, response_region, emission_region)
 
     if response_region == 'All regions':
 
+        region_names = input_selection.get_response_regions()
+        n_regions = len(region_names)
+
         artp_total_std = []
         slow_arpp_total_std = []
         fast_arpp_total_std = []
-        for i in range(input_selection.select_response_region('len')):
+
+        for i in range(n_regions):
 
             # The following needs to be multiplied by the total function to obtain the final uncertainty
             artp_total_std.append(
