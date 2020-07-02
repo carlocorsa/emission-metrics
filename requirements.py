79579c9decb73c167f1fa69f838e290ecb870922
carlocorsa <carlo.corsaro@gmail.com>
2020-07-02 17:34:12 +0100

    Added seaborn to requirements

--- requirements.py
+++ requirements.py
@@ -3,4 +3,5 @@ pip install scipy
 pip install netCDF4
 pip install matplotlib
 pip install pandas
+pip install seaborn
 
--- requirements_with_version.py
+++ requirements_with_version.py
@@ -3,4 +3,5 @@ pip install scipy==1.4.1
 pip install netCDF4==1.5.3
 pip install matplotlib==3.1.3
 pip install pandas==1.0.5
+pip install seaborn==0.10.1
 
