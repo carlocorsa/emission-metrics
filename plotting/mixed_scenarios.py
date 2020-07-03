ebbc320050e211349158d017382f524ec1d631c1
carlocorsa <carlo.corsaro@gmail.com>
2020-07-03 14:38:36 +0100

    Added plot labels, descriptions and other details

--- plotting/mixed_scenarios.py
+++ plotting/mixed_scenarios.py
@@ -8,13 +8,13 @@ from plotting import plot_utils
 from utils import constants
 
 # Font size
-FS = 13
+FS = 14
 
 # Line width
 LW = 3
 
 # y-axis limit factor
-LF = 0.2
+LF = 0.3
 
 # Plot parameters
 PARAMS = {
@@ -30,11 +30,27 @@ PARAMS = {
 }
 
 
-def plot_temp_mixed_scenario(temp_dict, time_horizons, time_step=0.01):
+def plot_temp_mixed_scenario(
+        temp_dict, time_horizons, scenarios, magnitudes, emission_region, response_region, time_step=0.01
+):
+
+    # Get name of pollutants
+    pollutants = list(temp_dict.keys())
 
     # Compute total response and uncertainty
-    tot_temp = sum(temp_dict[pol]['temp'] for pol in temp_dict.keys())
-    tot_std = np.sqrt(sum(temp_dict[pol]['std']**2 for pol in temp_dict.keys()))
+    tot_temp = sum(temp_dict[pol]['temp'] for pol in pollutants)
+    tot_std = np.sqrt(sum(temp_dict[pol]['std']**2 for pol in pollutants))
+
+    # Get minimum and maximum temperature change
+    min_temp = min(
+        min(min(temp_dict[pol]['temp']) for pol in pollutants),
+        min(tot_temp)
+    )
+
+    max_temp = max(
+        max(max(temp_dict[pol]['temp']) for pol in pollutants),
+        max(tot_temp)
+    )
 
     # Get total number of points
     n_points = len(tot_temp)
@@ -45,8 +61,8 @@ def plot_temp_mixed_scenario(temp_dict, time_horizons, time_step=0.01):
     # Plot temperature change for each pollutant
     plt.figure(figsize=(12, 8))
 
-    for i, pol in enumerate(temp_dict.keys()):
-        plt.plot(temp_dict[pol]['temp'], PARAMS[i]['color'], linewidth=LW)
+    for i, pol in enumerate(pollutants):
+        plt.plot(temp_dict[pol]['temp'], PARAMS[i]['color'], linewidth=LW, label=constants.NOTATIONS[pol])
         plt.fill_between(
             range(n_points),
             temp_dict[pol]['temp'] + temp_dict[pol]['std'],
@@ -55,8 +71,28 @@ def plot_temp_mixed_scenario(temp_dict, time_horizons, time_step=0.01):
             alpha=0.5
         )
 
+        # Get strings to use for the plot description
+        change_strings = plot_utils.get_change_strings(magnitudes[pol])
+
+        # Add scenarios information
+        if pol == "SO2":
+            y_padding = min_temp - LF + 0.14
+        else:
+            y_padding = max_temp + LF - 0.14
+
+        plt.text(1 / time_step, y_padding, '{} {} {}%'.format(
+            scenarios[0], change_strings[0], magnitudes[pol][0]
+        ), fontsize=FS-1, color=PARAMS[i]['color'], fontweight='bold')
+        for j, scenario in enumerate(scenarios[1:]):
+            plt.text((time_horizons[j] + 1) / time_step, y_padding, '{} {} {}%'.format(
+                scenario, change_strings[j+1], magnitudes[pol][j+1]
+            ), fontsize=FS-1, color=PARAMS[i]['color'], fontweight='bold')
+
     # Plot total temperature change
-    plt.plot(tot_temp, sns.xkcd_rgb["jungle green"], linewidth=LW)
+    plt.plot(
+        tot_temp, sns.xkcd_rgb["jungle green"], linewidth=LW,
+        label=' + '.join([constants.NOTATIONS[pol] for pol in pollutants])
+    )
     plt.fill_between(
         range(n_points),
         tot_temp + tot_std,
@@ -65,17 +101,7 @@ def plot_temp_mixed_scenario(temp_dict, time_horizons, time_step=0.01):
         alpha=0.5
     )
 
-    # Get minimum and maximum temperature change
-    min_temp = min(
-        min(min(temp_dict[pol]['temp']) for pol in temp_dict.keys()),
-        min(tot_temp)
-    )
-
-    max_temp = max(
-        max(max(temp_dict[pol]['temp']) for pol in temp_dict.keys()),
-        max(tot_temp)
-    )
-
+    # Define y-axis limits based on minimum and maximum values of temperature change
     y_limits = [np.round((min_temp-LF)*10) / 10, np.round((max_temp+LF)*10) / 10]
 
     # Plot lines for the time horizons
@@ -91,16 +117,12 @@ def plot_temp_mixed_scenario(temp_dict, time_horizons, time_step=0.01):
     plt.xlabel('Time (yr)', fontsize=FS)
     plt.xticks(xt, xtl, fontsize=FS)
     plt.ylim(y_limits)
+    plt.ylabel(r'$\Delta$T (K)', fontsize=FS)
+    plt.yticks(fontsize=FS)
 
-#     plt.ylabel(p_label, fontsize=fs)
-#
-#     plt.ylim(-0.45,1.30)
-# #    plt.legend(loc='best',fontsize=fs)
-#     plt.text(200,1.20,'linear increase up to 120%',fontsize=fs-1)
-#     plt.text(3100,1.20,'sustained emission at 120%',fontsize=fs-1)
-#     plt.text(6600,1.20,'linear decrease down to 100%',fontsize=fs-1)
-#     plt.text(1500,0.4, pol1, fontsize=25)
-#     plt.text(200,-0.40,'linear increase up to 150%',fontsize=fs-1)
-#     plt.text(3100,-0.40,'sustained emission at 150%',fontsize=fs-1)
-#     plt.text(6600,-0.40,'linear decrease down to 100%',fontsize=fs-1)
-#     plt.text(1500,-0.25, pol2, fontsize=25)
+    # Plot legend and title
+    plt.legend(loc='center right', fontsize=FS, facecolor='white')
+    title = "{} T change due to changes in {} emissions from {}".format(
+        response_region, " and ".join(pollutants), emission_region
+    )
+    plt.title(title, fontsize=FS + 2)
