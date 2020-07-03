0034212b4c86b8a14085db02e70b9e14db972e27
carlocorsa <carlo.corsaro@gmail.com>
2020-07-03 15:09:50 +0100

    Added bash script for the automatic creation of the conda environment

---
+++ install_conda_env.sh
@@ -0,0 +1,9 @@
+conda create -n rem python=3.6
+source activate rem
+conda install numpy=1.18.4
+conda install scipy=1.4.1
+conda install netCDF4=1.5.3
+conda install matplotlib=3.1.3
+conda install pandas=1.0.5
+conda install seaborn=0.10.1
+
