89478d812c3711110c0b57c2b0df0e342f525aa5
carlocorsa <carlo.corsaro@gmail.com>
2020-08-06 18:46:15 +0200

    Changed plot_lifetime_range to include ratio of potentials

--- plotting/sensitivity.py
+++ plotting/sensitivity.py
@@ -1,4 +1,5 @@
 # Third party imports
+import numpy as np
 import seaborn as sns
 from matplotlib import pyplot as plt
 
@@ -112,40 +113,58 @@ def plot_lifetime_range(artp_potentials, iartp_potentials, time_horizon):
     xt, xtl = plot_utils.optimal_xticks(time_horizon, n_points)
 
     # Plot style
-    sns.set_style("darkgrid")
+    sns.set_style("white")
 
     # Plot potentials using different lifetime values
     plt.figure(figsize=(12, 5))
 
     # Plot ARTP
-    plt.subplot(1, 2, 1)
-    plt.plot(artp_potentials['avg'], linewidth=LW, color='C0')
-    plt.fill_between(range(n_points), artp_potentials['min'], artp_potentials['max'], facecolor='C0', alpha=0.5)
-    plt.title("Pulse ARTP", fontsize=FS+2)
-    plt.plot([0, n_points], [0, 0], color='k', linestyle='--', linewidth=1)
+    ax1 = plt.subplot(1, 2, 1)
+    ax1.plot(artp_potentials['avg'], linewidth=LW, color='C0')
+    ax1.fill_between(range(n_points), artp_potentials['min'], artp_potentials['max'], facecolor='C0', alpha=0.5)
+    ax1.set_title("Pulse ARTP", fontsize=FS+2)
+    # ax1.plot([0, n_points], [0, 0], color='k', linestyle='--', linewidth=1)
 
     # Set axis ticks, labels and limits
-    plt.xlim([0, n_points])
-    plt.xlabel('Time (yr)', fontsize=FS)
+    ax1.set_xlim([0, n_points])
+    ax1.set_xlabel('Time (yr)', fontsize=FS)
     plt.xticks(xt, xtl, fontsize=FS)
-    plt.ylabel('ARTP (K Tg$^{{-1}}$)', fontsize=FS)
-    plt.yticks(fontsize=FS)
-    plt.gca().get_yaxis().get_major_formatter().set_powerlimits((0, 0))
+    if max(artp_potentials['avg']) < 0:
+        ax1.set_ylim([ax1.get_ylim()[0], 0])
+    else:
+        ax1.set_ylim([0, ax1.get_ylim()[1]])
+    ax1.set_ylabel('ARTP (K Tg$^{{-1}}$)', fontsize=FS)
+    ax1.tick_params(labelsize=FS)
+    ax1.get_yaxis().get_major_formatter().set_powerlimits((0, 0))
+
+    # Add second axis with ratio of potentials
+    ax2 = ax1.twinx()
+    ax2.plot(np.array(artp_potentials['avg']) / np.array(artp_potentials['min']), color='C1')
+    ax2.set_ylim([1, 1.4])
 
     # Plot iARTP
-    plt.subplot(1, 2, 2)
-    plt.plot(iartp_potentials['avg'], linewidth=LW, color='C0')
-    plt.fill_between(range(n_points), iartp_potentials['min'], iartp_potentials['max'], facecolor='C0', alpha=0.5)
-    plt.title("Sustained ARTP", fontsize=FS+2)
-    plt.plot([0, n_points], [0, 0], color='k', linestyle='--', linewidth=1)
+    ax3 = plt.subplot(1, 2, 2)
+    ax3.plot(iartp_potentials['avg'], linewidth=LW, color='C0')
+    ax3.fill_between(range(n_points), iartp_potentials['min'], iartp_potentials['max'], facecolor='C0', alpha=0.5)
+    ax3.set_title("Sustained ARTP", fontsize=FS+2)
+    # ax3.plot([0, n_points], [0, 0], color='k', linestyle='--', linewidth=1)
 
     # Set axis ticks, labels and limits
-    plt.xlim([0, n_points])
-    plt.xlabel('Time (yr)', fontsize=FS)
+    ax3.set_xlim([0, n_points])
+    ax3.set_xlabel('Time (yr)', fontsize=FS)
     plt.xticks(xt, xtl, fontsize=FS)
-    plt.ylabel('iARTP (K Tg$^{{-1}}$)', fontsize=FS)
-    plt.yticks(fontsize=FS)
-    plt.gca().get_yaxis().get_major_formatter().set_powerlimits((0, 0))
+    if max(artp_potentials['avg']) < 0:
+        ax3.set_ylim([ax3.get_ylim()[0], 0])
+    else:
+        ax3.set_ylim([0, ax3.get_ylim()[1]])
+    ax3.set_ylabel('iARTP (K Tg$^{{-1}}$)', fontsize=FS)
+    ax3.tick_params(labelsize=FS)
+    ax3.get_yaxis().get_major_formatter().set_powerlimits((0, 0))
+
+    # Add second axis with ratio of potentials
+    ax4 = ax3.twinx()
+    ax4.plot(np.array(iartp_potentials['avg']) / np.array(iartp_potentials['min']), color='C1')
+    ax4.set_ylim([1, 1.4])
 
     # Adjust subplots position
     plt.subplots_adjust(top=0.90, bottom=0.10, left=0.10, right=0.95, hspace=0.25, wspace=0.28)
