ce0a67b74d0318ca335a8d470d26cabe6ea598b3
carlocorsa <carlo.corsaro@gmail.com>
2020-08-06 16:38:11 +0200

    Added standard deviation of pollutant lifetimes

--- utils/constants.py
+++ utils/constants.py
@@ -1,3 +1,5 @@
+import numpy as np
+
 # List of available pollutants
 POLLUTANTS = ['SO2', 'BC', 'CO2', 'CH4']
 
@@ -49,6 +51,8 @@ SPECS = {
 
     'SO2': {
         'tau': 4.35 / 365,  # Wang et al. (2013) (SO2 -> 0.25) and Fioletov et al. (2015) (SO4 -> 4.1),
+        'tau_std': np.sqrt((0.08 / 365) ** 2 + (0.7 / 365) ** 2),  # SO2: Fioletov et al. (2015),
+                                                                   # SO4: Textor et. al (2006)
         'fp': -0.4,  # Kvalevag et al. (2013) used by Shine et al. (2015),
         'k': 2.678,  # All k values were given by Bjorn from his paper Samset et al. (2018)
         'k_std': 0.317
@@ -56,6 +60,7 @@ SPECS = {
 
     'BC': {
         'tau': 6.8 / 365,  # Schulz et al. (2006)
+        'tau_std': 1.8/365,  # Schulz et al. (2006)
         'fp': 1,  # because we are using RFa instead of RF -
                   # otherwise use 6.2 from Kvalevag et al. (2013) used by Shine et al. (2015)
         'k': 2.675,
@@ -74,9 +79,10 @@ SPECS = {
 
     'CH4': {
         'f': 1.24,  # from Voulgarakis et al. (2012) # Collins et al. (2013) -> 1.34
-        'tau': 9.7 * 1.24,  # std = 1.5 from Naik et al. (2013) (1.24 is the factor f)
+        'tau': 9.7 * 1.24,  # (1.24 is the factor f)
                             # check also 9.1 * f -std = 0.9 from Prather et al. (2012)
                             # check also Collins et al. (2013) -> 8.7 * f and Myhre et al. (2013) -> 12.4 yr
+        'tau_std': 1.5,  # Naik et al. (2013)
         'fp': 1,  # because we are using RFa instead of RF
                   # Otherwise use 0.3 from Kvalevag et al. (2013) used by Shine et al. (2015)
         'k': 2.766 ,
